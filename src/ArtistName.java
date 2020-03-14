a5e8fad55e1aa6efb990c56ccad5e81c78465987
itsnek <45695803+itsnek@users.noreply.github.com>
2020-03-14 20:05:42 +0200

    initial commit

---
+++ src/ArtistName.java
@@ -0,0 +1,4 @@
+public class ArtistName {
+    String artistName;
+
+}
---
+++ src/MusicFile.java
@@ -0,0 +1,8 @@
+public class MusicFile {
+    String trackName;
+    String artistName;
+    String albumInfo;
+    String genre;
+    String musicFileExtract;
+
+}
