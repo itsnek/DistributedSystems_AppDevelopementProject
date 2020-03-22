e5dc07540a2311f9d367c0a04c8fd9df34c758b8
ariskou1999 <62185420+ariskou1999@users.noreply.github.com>
2020-03-22 17:37:01 +0200

    Add files via upload

---
+++ src/Node.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class Node {
+
+    List<Broker> brokers;
+
+    public void init(){
+
+
+
+    }
+    public ArrayList<Broker> getBrokers(){
+
+
+
+    }
+    public void connect(){
+
+
+
+    }
+    public abstract void disconnect(){
+
+
+
+    }
+    public void updateNodes(){
+
+
+    }
+
+}
