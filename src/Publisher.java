dd2a1796a0e2d52eea36940a4512ae5658e3d4c6
axill12 <axilleas12@gmail.com>
2020-03-25 00:05:09 +0200

    commit Publisher.java

--- src/Publisher.java
+++ src/Publisher.java
@@ -1,31 +1,54 @@
-public class Publisher extends Thread {
-    private List <ArtistName> Artists;
-    private List <MusicFile> Songs;
-    private Socket clientSocket = null;
-    private ObjectOutputStream out = null;
-    private ObjectInputStream in = null;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
-    public void init (List <ArtistName> artists, List<MusicFile> songs) {
+public class Publisher {
+    private static ArrayList <ArtistName> Artists = new ArrayList<ArtistName> (30);
+    private static ArrayList <MusicFile> Songs = new ArrayList<MusicFile> (300);
+    private static ArrayList<ServerSocket> serverSockets = new ArrayList<ServerSocket>();
+    private static ArrayList<Socket> clientSockets = new ArrayList<Socket>();
+    private static ArrayList<ObjectOutputStream> out = new ArrayList<ObjectOutputStream>();
+    private static ArrayList<ObjectInputStream> in = new ArrayList<ObjectInputStream>();
+    private static final int startingSocketNumber = 1050;
+    private static int socketCounter = 0; // It counts total sockets.
+    private static int clientSocketCounter = 0; //It counts only sockets of clients.
+    private static int serverSocketCounter = 0; //It counts only sockets of servers.
+
+    public void init (ArrayList <ArtistName> artists, ArrayList<MusicFile> songs) {
         Artists = artists;
         Songs = songs;
-    }
 
-    public void connect () {
+        for (int i=0; i < serverSockets.size(); i++) {
+            serverSockets.add (null);
+        }
+
+        for (int i=0; i < clientSockets.size(); i++) {
+            clientSockets.add (null);
+        }
+    }
+    // Create client side connection.
+    public void connectToServer (String serverIp) {
         try {
-            clientSocket = new Socket("127.0.0.1", 1050);
-            out = new ObjectOutputStream(clientSocket.getOutputStream());
-            in = new ObjectInputStream(clientSocket.getInputStream());
+            clientSockets.set(socketCounter, new Socket(serverIp, startingSocketNumber + socketCounter));
+            out.set(socketCounter, new ObjectOutputStream(clientSockets.get(socketCounter).getOutputStream()));
+            in.set(socketCounter, new ObjectInputStream(clientSockets.get(socketCounter).getInputStream()));
+            socketCounter++;
+            clientSocketCounter++;
         } catch (UnknownHostException uhe) {
             System.err.println ("You are trying to connect to an unknown host.");
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
-    public void disconnect () {
+    public void disconnect (int connectionToClose) {
         try {
-            in.close();
-            out.close();
-            clientSocket.close();
+            in.get(connectionToClose).close();
+            out.get(connectionToClose).close();
+            serverSockets.get(connectionToClose).close();
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
