842a76766c3049274f9a059c7aee81ae81e7d7da
axill12 <axilleas12@gmail.com>
2020-03-27 23:31:52 +0200

    commit Publisher.java and PublisherThread.java

--- src/Publisher.java
+++ src/Publisher.java
@@ -1,3 +1,4 @@
+import com.mpatric.mp3agic.*;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
@@ -5,6 +6,7 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.io.File;
 
 public class Publisher {
     private ArrayList <ArtistName> Artists = new ArrayList<ArtistName> (30);
@@ -20,7 +22,8 @@ public class Publisher {
     private int serverSocketCounter = 0; //It counts only sockets of servers.*/
 
     public void init () {
-
+        ArrayList<String> songs = getSongNames();
+        ArrayList<ArtistName> artistNames = findArtistForEachSong(songs); // It contains the name of artist of each song. So many names are being repeated because many songs have same artist.
 
         serverSocket = null;
 
@@ -44,6 +47,54 @@ public class Publisher {
                 ioe.printStackTrace();
             }
         }
+    }
+
+    private ArrayList<String> getSongNames () {
+        File file = new File ("C:/Users/User/Desktop/Distributed Systems/Project/dataset2/dataset2");
+        File [] list = file.listFiles(); // h arxikh lista pou periexei ola ta arxeia
+        String [] finalList = new String [list.length]; // h lista pou periexei mono ta tragoydia
+        String [] rawSongs; //= new String [list.length]; vriskontai mesa kai ta arxeia pou arxizoun me ._
+        ArrayList<String> songs = new ArrayList<String>(100);
+        for (int i=0; i<list.length; i++) {
+            if (list[i].isDirectory()) {
+                rawSongs = list[i].list();
+                for (int j=0; j<rawSongs.length; j++) {
+                    if (!rawSongs[j].startsWith("._") && rawSongs[j].endsWith(".mp3")) {
+                        songs.add(rawSongs[j]);
+                    }
+                }
+            } else if (!list[i].getName().startsWith("._") && list[i].getName().endsWith(".mp3")) {
+                songs.add(list[i].getName());
+            }
+        }
+        return songs;
+    }
+
+    // This method finds artist name for each song in the song list.
+    private ArrayList<ArtistName> findArtistForEachSong (ArrayList<String> songs) {
+        Mp3File mp3File;
+        ID3v1 id3v1Tag;
+        ID3v2 id3v2Tag;
+        ArrayList<ArtistName> artistNames = new ArrayList<ArtistName>(100); // It contains the name of artist of each song. So many names are being repeated because many songs have same artist.
+        for (int i=0; i<songs.size(); i++) {
+            try {
+                mp3File = new Mp3File (songs.get(i));
+                if (mp3File.hasId3v1Tag()) {
+                    id3v1Tag = mp3File.getId3v1Tag();
+                    artistNames.add( new ArtistName( id3v1Tag.getArtist() ) );
+                } else if (mp3File.hasId3v2Tag()) {
+                    id3v2Tag = mp3File.getId3v2Tag();
+                    artistNames.add( new ArtistName( id3v2Tag.getArtist() ) );
+                }
+            } catch (UnsupportedTagException unsTag) { // I put catch here so lines after "mp3File = new Mp3File (songs.get(i));" be not executed and program proceed to next repetition in for loop.
+
+            } catch (InvalidDataException invData) { // I put catch here so lines after "mp3File = new Mp3File (songs.get(i));" be not executed and program proceed to next repetition in for loop.
+
+            } catch (IOException ioe) { // I put catch here so lines after "mp3File = new Mp3File (songs.get(i));" be not executed and program proceed to next repetition in for loop.
+
+            }
+        }
+        return artistNames;
     }
 }
 
