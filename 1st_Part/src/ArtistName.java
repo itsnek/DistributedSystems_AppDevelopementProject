5b0904f88d938344d3ed4f6eb25b0dd9bd68490d
itsnek <45695803+itsnek@users.noreply.github.com>
2020-04-29 12:46:38 +0300

    New format

---
+++ 1st_Part/src/ArtistName.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+public class ArtistName implements Serializable {
+    String artistName;
+
+    ArtistName(String artistName){
+        this.artistName = artistName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+}
---
+++ 1st_Part/src/Value.java
@@ -0,0 +1,12 @@
+public class Value {
+
+    MusicFile musicFile;
+
+    Value(MusicFile msc){
+        musicFile = msc;
+    }
+
+    public MusicFile getValue() {
+        return musicFile;
+    }
+}
