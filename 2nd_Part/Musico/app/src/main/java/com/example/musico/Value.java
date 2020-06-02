ec74827508e57492956557be27927a893f416ea0
axill12 <61993544+axill12@users.noreply.github.com>
2020-06-02 13:37:56 +0300

    online

---
+++ 2nd_Part/Musico/app/src/main/java/com/example/musico/Value.java
@@ -0,0 +1,14 @@
+package com.example.musico;
+
+public class Value {
+
+    MusicFile musicFile;
+
+    Value(MusicFile msc){
+        musicFile = msc;
+    }
+
+    public MusicFile getValue() {
+        return musicFile;
+    }
+}
