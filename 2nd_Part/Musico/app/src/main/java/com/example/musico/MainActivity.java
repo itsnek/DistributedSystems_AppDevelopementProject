256e5349b37213fd2cf034200f9b4909614179d6
axill12 <61993544+axill12@users.noreply.github.com>
2020-06-04 13:57:25 +0300

    online

--- 2nd_Part/Musico/app/src/main/java/com/example/musico/MainActivity.java
+++ 2nd_Part/Musico/app/src/main/java/com/example/musico/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e("MainActivity", "at start");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
